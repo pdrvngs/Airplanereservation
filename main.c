f7379e8b3bda3538c7fc3296d07d207e01ff302d
Luis Venegas <luisvenegas@ufm.edu>
2020-03-11 18:58:21 -0600

    Adds menu function

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+// Global Variables
+char flightid;
+
+
+int menu(){
+    int choice = 0;
+    printf("1. Reservar Asiento\n2. Disponibilidad de Asientos\n3. Salir \n");
+    while(choice != 3){
+        printf("Ingrese su eleccion: ");
+        scanf("%d", &choice);
+        switch(choice) {
+            case 1:
+                printf("Eligio 1");
+                return 1;
+            case 2:
+                printf("Eligio 2");
+                return 2;
+
+        }
+    }
+}
+
 int main() {
-    printf("Hello, World!\n");
+    menu();
     return 0;
 }
