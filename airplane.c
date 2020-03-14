d99ec27e9e2ae19720c27f1176ee9f8f4f1126eb
Luis Venegas <luisvenegas@ufm.edu>
2020-03-14 13:41:02 -0600

    Adds review functionality, pressenter function and cleans up code. Project is complete in functionality, just needs extra checks and validations, missing extra features

--- airplane.c
+++ airplane.c
@@ -8,6 +8,7 @@
 
 // Function prototypes
 void clearscreen(void); // Clears screen
+void pressenter(void); // PRESS ENTER
 int menu(void); // Provides menu options and checks input
 void flightidchecker(void); // checks flight id for correct # of digits
 void initializer(void); // Initializes flight seat array
@@ -15,6 +16,7 @@ int getrownumber(void); // gets row number based on input [letter-num-num or let
 int getcolnumber(void); // gets col number based on the first value of input
 void reservation(void); // assigns reservation to seat number given, runs check for reserved seats
 void available(void); // displays the seat chart with available seats.
+void resumen(void); //
 
 
 // Global Variables
@@ -29,45 +31,11 @@ void clearscreen()
 }
 
 
-int menu(){
-    clearscreen();
+void pressenter(void){
+    char nothing[3];
     fflush(stdin);
-    int choice = 0;
-    printf("VUELO: "); fputs(flightid, stdout); printf("\n");
-    printf("1. Reservar Asiento\n2. Disponibilidad de Asientos\n3. Salir \n");
-    while(choice != 3){
-        printf("Ingrese su eleccion: ");
-        scanf("%d", &choice);
-        switch(choice) {
-            case 1:
-                return 1;
-            case 2:
-                return 2;
-
-            default:
-                if(choice != 3) {
-                    printf("Oops, that's not a valid choice!\n");
-                    fflush(stdin);
-                }
-        }
-    }
-    return 3;
-}
-
-
-void flightidchecker(){
-    fflush(stdin);
-
-    printf("Welcome! Please enter your flight code: ");
-    fgets(flightid, 10, stdin);
-    unsigned int num = strlen(flightid);
-    while(num != 6){
-        fflush(stdin);
-        printf("No es un vuelo valido, porfavor intente de nuevo: ");
-        fgets(flightid, 10, stdin);
-        num = strlen(flightid);
-    }
-    clearscreen();
+    printf("Presione ENTER para regresar al menu principal\n");
+    fgets(nothing, 3, stdin);
 }
 
 
@@ -121,13 +89,56 @@ int getcolnumber(){
 
 }
 
+
+int menu(){
+    clearscreen();
+    fflush(stdin);
+    int choice = 0;
+    printf("VUELO: "); fputs(flightid, stdout); printf("\n");
+    printf("1. Reservar Asiento\n2. Disponibilidad de Asientos\n3. Resumen\n4. Salir \n");
+    while(choice != 4){
+        printf("Ingrese su eleccion: ");
+        scanf("%d", &choice);
+        switch(choice) {
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            case 3:
+                return 3;
+
+            default:
+                if(choice != 4) {
+                    printf("Oops, that's not a valid choice!\n");
+                    fflush(stdin);
+                }
+        }
+    }
+    return 4;
+}
+
+
+void flightidchecker(){
+    fflush(stdin);
+
+    printf("Welcome! Please enter your flight code: ");
+    fgets(flightid, 10, stdin);
+    unsigned int num = strlen(flightid);
+    while(num != 6){
+        fflush(stdin);
+        printf("No es un vuelo valido, porfavor intente de nuevo: ");
+        fgets(flightid, 10, stdin);
+        num = strlen(flightid);
+    }
+    clearscreen();
+}
+
 // is missing the type menu to return to menu from FASE II
 void reservation() {
     fflush(stdin);
     int reservado = 0;
     int row;
     int col;
-    char nothing[3];
     char* pinput = input;
 
     // Asks for seat number
@@ -151,9 +162,9 @@ void reservation() {
             reservado = 1;
         }
     }
-    fflush(stdin);
-    fgets(nothing, 3, stdin);
+    pressenter();
 }
+
 
 void available(){
     fflush(stdin);
@@ -176,6 +187,33 @@ void available(){
     fgets(nothing, 3, stdin);
 }
 
+
+void resumen(void){
+    float counter = 0;
+    float reservedper = 0;
+    float availableper = 0;
+    int reserved = 0;
+    int available = 0;
+
+    for(int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            if(flightseats[i][j] == 'X'){
+                counter++;
+            }
+        }
+    }
+
+    reserved = counter;
+    available = 192 - reserved;
+    reservedper = counter/192;
+    availableper = 1 - reservedper;
+
+    printf("Reserved: %d/192 | [%f%%]\n", reserved, reservedper);
+    printf("Available: %d/192 | [%f%%]\n", available, availableper);
+
+    pressenter();
+}
+
 int main() {
     fflush(stdin);
     clearscreen();
@@ -187,8 +225,10 @@ int main() {
     // Checks flight number
     flightidchecker();
 
+
+
     // menu function
-    while(choice != 3){
+    while(choice != 4){
         choice = menu();
         if(choice == 1){
             // calls reservation process
@@ -197,6 +237,9 @@ int main() {
         else if(choice == 2){
             // gets available seats
             available();
+        }
+        else if(choice == 3){
+            resumen();
         }
 
     }
