dda3ad477027620924213ffde84aeaa81227a107
Luis Venegas <luisvenegas@ufm.edu>
2020-03-14 18:25:32 -0600

    Reinforces reservation input with upper/lower checks and adds type menu to exit functionality

--- airplane.c
+++ airplane.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 // MACROS
 #define ROWS 32
@@ -8,16 +9,16 @@
 
 // Function prototypes
 void clearscreen(void); // Clears screen
-void pressenter(void); // PRESS ENTER
-int menu(void); // Provides menu options and checks input
-void flightidchecker(void); // checks flight id for correct # of digits
-void initializer(void); // Initializes flight seat array
+void pressenter(void); // PRESS ENTER to continue
 int getrownumber(void); // gets row number based on input [letter-num-num or letter-num]
 int getcolnumber(void); // gets col number based on the first value of input
-void reservation(void); // assigns reservation to seat number given, runs check for reserved seats
+void initializer(void); // Initializes flight seat array
+void flightidchecker(void); // checks flight id for correct # of digits
+void seatchecker(void); // checks that seat reservation number is valid
 void available(void); // displays the seat chart with available seats.
-void resumen(void); //
-
+void reservation(void); // assigns reservation to seat number given, runs check for reserved seats
+void resumen(void); // displays the overview style seat reservation
+int menu(void); // Provides menu options and checks input
 
 // Global Variables
 char flightid[10];
@@ -51,19 +52,31 @@ void initializer(){
 }
 
 
+char tolowerstring(char string[255]){
+    for(int i = 0; string[i]; i++){
+        string[i] = tolower(string[i]);
+    }
+    return *string;
+}
+
+
+char toupperstring(char string[255]){
+    for(int i = 0; string[i]; i++){
+        string[i] = toupper(string[i]);
+    }
+    return *string;
+}
+
+
 int getrownumber(){
     fflush(stdin);
-
     char* pinput = input;
     int int1;
     int int2;
     int row;
-
-
     unsigned int length = strlen(pinput);
 
     if(length == 4){
-
         int1 = (pinput[1]-48)*10;
         int2 = (pinput[2] - 48);
         row = int1 + int2;
@@ -72,49 +85,16 @@ int getrownumber(){
     } else if(length == 3){
         int1 = (pinput[1] - 48);
         return int1 - 1;
-    }
-    return 0;
+    }else {return 0;}
 }
 
 
 int getcolnumber(){
     fflush(stdin);
-
     char* pinput = input;
     int column;
-
-
-    column = pinput[0] - 65;
+    column = toupper(pinput[0]) - 65;
     return column;
-
-}
-
-
-int menu(){
-    clearscreen();
-    fflush(stdin);
-    int choice = 0;
-    printf("VUELO: "); fputs(flightid, stdout); printf("\n");
-    printf("1. Reservar Asiento\n2. Disponibilidad de Asientos\n3. Resumen\n4. Salir \n");
-    while(choice != 4){
-        printf("Ingrese su eleccion: ");
-        scanf("%d", &choice);
-        switch(choice) {
-            case 1:
-                return 1;
-            case 2:
-                return 2;
-            case 3:
-                return 3;
-
-            default:
-                if(choice != 4) {
-                    printf("Oops, that's not a valid choice!\n");
-                    fflush(stdin);
-                }
-        }
-    }
-    return 4;
 }
 
 
@@ -133,35 +113,45 @@ void flightidchecker(){
     clearscreen();
 }
 
-// is missing the type menu to return to menu from FASE II
+
 void reservation() {
     fflush(stdin);
     int reservado = 0;
-    int row;
-    int col;
+    int row = 40;
+    int col = 10;
     char* pinput = input;
 
     // Asks for seat number
     printf("Que asiento desea reservar? \n");
-    fgets(pinput, 5, stdin);
-    col = getcolnumber();
-    printf("\n");
-    row = getrownumber();
-
-    while(reservado != 1) {
-        if(flightseats[row][col] == 'X'){
-            printf("Este asiento ya esta reservado, porfavor ingrese otro asiento: ");
-            fgets(pinput, 5, stdin);
+    //while((col > 6 || col < 0) && (row > 32 || row < 0)) {
+        fgets(pinput, 5, stdin);
+        tolowerstring(pinput);
+        if(strcmp(pinput, "menu") == 0){
+            return;
+        }else {
+            toupperstring(pinput);
             col = getcolnumber();
-            printf("\n");
             row = getrownumber();
+            //printf("%d %d", col, row);
+            /* if((col > 6 || col < 0) && (row > 32 || row < 0)){
+                 printf("No es un asiento valido, intente de nuevo: ");
+             }
+         }*/
+            while (reservado != 1) {
+                if (flightseats[row][col] == 'X') {
+                    printf("Este asiento ya esta reservado, porfavor ingrese otro asiento: ");
+                    fgets(pinput, 5, stdin);
+                    col = getcolnumber();
+                    printf("\n");
+                    row = getrownumber();
+                } else {
+                    printf("Felicitaciones! Reservo el asiento: ");
+                    fputs(pinput, stdout);
+                    flightseats[row][col] = 'X';
+                    reservado = 1;
+                }
+            }
         }
-        else {
-            printf("Felicitaciones! Reservo el asiento: "); fputs(pinput, stdout);
-            flightseats[row][col] = 'X';
-            reservado = 1;
-        }
-    }
     pressenter();
 }
 
@@ -213,6 +203,35 @@ void resumen(void){
 
     pressenter();
 }
+
+
+int menu(){
+    clearscreen();
+    fflush(stdin);
+    int choice = 0;
+    printf("VUELO: "); fputs(flightid, stdout); printf("\n");
+    printf("1. Reservar Asiento\n2. Disponibilidad de Asientos\n3. Resumen\n4. Salir \n");
+    while(choice != 4){
+        printf("Ingrese su eleccion: ");
+        scanf("%d", &choice);
+        switch(choice) {
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            case 3:
+                return 3;
+
+            default:
+                if(choice != 4) {
+                    printf("Oops, that's not a valid choice!\n");
+                    fflush(stdin);
+                }
+        }
+    }
+    return 4;
+}
+
 
 int main() {
     fflush(stdin);
